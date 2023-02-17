62e0906d42995a1b6142ba9330dbc4883d551f3b
Willem Loeven <willem@willemloeven.nl>
2023-02-17 08:40:20 +0100

    dded files

--- azure-custom-image/linux/scripts/installers/python.sh
+++ azure-custom-image/linux/scripts/installers/python.sh
@@ -10,25 +10,21 @@ source $HELPER_SCRIPTS/etc-environment.sh
 source $HELPER_SCRIPTS/os.sh
 
 # Install Python, Python 3, pip, pip3
-if isUbuntu18; then
-    apt-get install -y --no-install-recommends python python-dev python-pip
-fi
-
-apt-get install -y --no-install-recommends python3 python3-dev python3-pip python3-venv
-
+apt-get install -y python3 python3-dev python3-pip python3-venv
+# fix apt
+apt --fix-broken install
 # Install pipx
 # Set pipx custom directory
 export PIPX_BIN_DIR=/opt/pipx_bin
 export PIPX_HOME=/opt/pipx
-python3 -m pip install pipx
-python3 -m pipx ensurepath
+#python3 -m pip install pipx
+#python3 -m pipx ensurepath
 # Update /etc/environment
 setEtcEnvironmentVariable "PIPX_BIN_DIR" $PIPX_BIN_DIR
 setEtcEnvironmentVariable "PIPX_HOME" $PIPX_HOME
 prependEtcEnvironmentPath $PIPX_BIN_DIR
 
-# fix apt
-apt --fix-broken install
+
 # Test pipx
 if ! command -v pipx; then
     echo "pipx was not installed or not found on PATH"
